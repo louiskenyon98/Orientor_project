a437c2a9c414f528018c3d3aa62a3f16c04c3f63
philippe beliveau <philbeliveau@hotmail.com>
2025-04-26 21:29:53 -0400

    api health

--- backend/app/main.py
+++ backend/app/main.py
@@ -102,7 +102,7 @@ logger.info("======================")
 def read_root():
     return {"message": "Welcome to the Orientor API"}
 
-@app.get("/health") # /api/health
+@app.get("/api/health") # /api/health
 def health_check():
     try:
         return {"status": "ok"}
