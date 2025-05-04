5d246a8adab7f9fc66c7f8036b70fb8bbbe42399
philippe beliveau <philbeliveau@hotmail.com>
2025-05-04 12:00:39 -0400

    opacity not working

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -38,11 +38,11 @@ export default function RootLayout({
         <body className="min-h-screen bg-white dark:bg-black text-neutral-700 dark:text-gray-200 antialiased">
           {/* Background patterns */}
           <div 
-            className="fixed inset-0 -z-10 bg-[url('/patterns/branch.svg')] bg-repeat opacity-90 dark:opacity-20"
+            className="fixed inset-0 -z-10 bg-[url('/patterns/branch.svg')] bg-repeat opacity-90 dark:opacity-300"
             style={{ backgroundSize: '200px 200px' }}
           ></div>
           <div 
-            className="fixed inset-0 -z-10 bg-[url('/patterns/grid.svg')] bg-repeat opacity-90 dark:opacity-20"
+            className="fixed inset-0 -z-10 bg-[url('/patterns/grid.svg')] bg-repeat opacity-90 dark:opacity-300"
             style={{ backgroundSize: '200px 200px' }}
           ></div>
           
